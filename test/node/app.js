f61553257542d83e69cbf7fc56d967a8c5be9316
napicell <napicell@amazon.com>
2020-11-19 11:22:10 +0100

    Receive token and streamUrl as cli parameters

--- test/node/app.js
+++ test/node/app.js
@@ -6,52 +6,49 @@ const readline = require('readline');
 const ssm = require("../../ssm.js");
 
 const termOptions = {
-  rows: 34,
-  cols: 197
+	rows: 34,
+	cols: 197
 };
 
 (async () => {
-	var startSessionRes = await session();
-	//console.log(`TokenValue: ${startSessionRes.TokenValue}`)
-	//console.log(`StreamUrl: ${startSessionRes.StreamUrl}`)
+	const token = process.argv[2];
+	const streamUrl = process.argv[3];
 
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: null
-	});
 	readline.emitKeypressEvents(process.stdin);
+	if (process.stdin.isTTY)
+		process.stdin.setRawMode(true);
 	process.stdin.on('keypress', (str, key) => {
 		if (connection.readyState === connection.OPEN) {
 			ssm.sendText(connection, str);
 		}
 	});
-	
+
 	const WebSocket = require('ws')
-	const connection = new WebSocket(startSessionRes.StreamUrl)
+	const connection = new WebSocket(streamUrl)
 
 	connection.onopen = () => {
 		ssm.init(connection, {
-			token: startSessionRes.TokenValue,
+			token: token,
 			termOptions: termOptions
 		})
 	}
-	
+
 	connection.onerror = (error) => {
 		console.log(`WebSocket error: ${error}`)
 	}
 
 	connection.onmessage = (event) => {
 		var agentMessage = ssm.decode(event.data);
-		//console.log(agentMessage);
 		ssm.sendACK(connection, agentMessage);
-        if (agentMessage.payloadType === 1){
-          process.stdout.write(agentMessage.payload);
-        } else if (agentMessage.payloadType === 17){
-          ssm.sendInitMessage(connection, termOptions);
-        }
+		if (agentMessage.payloadType === 1){
+			process.stdout.write(agentMessage.payload);
+		} else if (agentMessage.payloadType === 17){
+			ssm.sendInitMessage(connection, termOptions);
+		}
 	}
 
 	connection.onclose = () => {
 		console.log("websocket closed")
+		process.exit(0);
 	}
 })();
